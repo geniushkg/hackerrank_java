c9df3b25ce6e43f72ae77bb875f12db58a0bd890
geniushkg <geniushkg@gmail.com>
2016-11-12 23:08:22 +0530

    feat : multiply table

---
+++ multiply table.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int x = 1 ; x<=10;x++){
+            System.out.println(N + " x " + x +" = "+(N*x));
+        }
+    }
+}
